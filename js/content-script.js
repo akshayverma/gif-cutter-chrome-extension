8d77926b9537519c1e5774d734af30bce1c4c542
akshay <veaksha@amazon.com>
2019-10-19 04:30:48 +0530

    Adding crappy ux to youtube videos to take start time and gif duration as input

--- js/content-script.js
+++ js/content-script.js
@@ -1,6 +1,26 @@
-function loadScript(scriptName, callback) {
-    var script = document.createElement('script');
-    script.src = chrome.extension.getURL(scriptName);
-    script.addEventListener('load', callback, false);
-    (document.head || document.documentElement).appendChild(script);
+var startTimeBox = document.createElement("input");
+startTimeBox.id = "startTime";
+startTimeBox.placeholder = "Enter start time";
+startTimeBox.style.cssText = "position:relative; width: 100px;border: solid 2px #2F4F4F;height: 26px;padding-left: 10px;";
+
+var deltaBox = document.createElement("input");
+deltaBox.id = "delta";
+deltaBox.placeholder = "Enter number of seconds < 3";
+deltaBox.style.cssText = "position:relative; left: 25px;width: 100px;border: solid 2px #2F4F4F;height: 26px;padding-left: 10px;";
+
+var submitBtn = document.createElement("button");
+submitBtn.id = "submitBtn";
+submitBtn.innerHTML = "crerate gif";
+submitBtn.style.cssText = "position:relative;left: 75px;width: 75px;";
+
+
+var setup = function() {
+  var parentElement = document.getElementById("info");
+  parentElement.appendChild(startTimeBox);
+  parentElement.appendChild(deltaBox);
+  parentElement.appendChild(submitBtn);
 }
+
+window.addEventListener('load', function() {
+    setup();
+});
