98444a90de914bcc0cbe04748288bb42a6b04bd6
akshay <veaksha@amazon.com>
2019-10-19 07:36:44 +0530

    -Integrated with GIFEncoder - https://github.com/antimatter15/jsgif - Naive Implementation using promise

--- js/content-script.js
+++ js/content-script.js
@@ -32,15 +32,26 @@ window.addEventListener('load', function() {
       var deltaTime = deltaBox.value;
       console.log(startTime);
       console.log(deltaTime);
-      var img = getImageAtTime(startTime);
-      outputDiv.appendChild(img);
+
+      if (!isValidGIFRequest(startTime, delta)){
+        alert('Invalid request');
+      }
+      createGIF(startTime, deltaTime);
     });
 });
+
+var isValidGIFRequest = function(startTime, delta) {
+  var video = document.getElementsByTagName("video")[0];
+  var videoDuration = video.duration;
+  if (startTime < 0 || delta < 0 ||(startTime + delta > videoDuration)) {
+    return false;
+  }
+  return true;
+}
 
 /***********Lib functions to get images and create GIF**********************/
 function getImageAtTime(secs) {
   var video = document.getElementsByTagName("video")[0];
-  console.log(video);
   var videoDuration = video.duration;
 
   if (secs > 0 && secs < videoDuration ) {
@@ -52,6 +63,49 @@ function getImageAtTime(secs) {
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     var img = new Image();
     img.src = canvas.toDataURL();
-    return img;
+    outputDiv.appendChild(img);
+    return ctx;
   }
+}
+
+var readFrames = function(timeStamps, images) {
+  return timeStamps.reduce((p, timeStamp) => {
+     return p.then(() => {
+       images.push(getImageAtTime(timeStamp));
+     });
+  }, Promise.resolve());
+};
+
+function createGIF(startTime, delta) {
+  var deltaInMS = delta * 1000;
+  var iter = 0;
+  var timeStamps = [];
+  var images = [];
+
+  while (iter < deltaInMS) {
+    timeStamps.push(startTime + iter/1000);
+    iter+=250;
+  }
+
+  readFrames(timeStamps, images);
+
+  const encoder = new GIFEncoder(320, 240);
+  encoder.start();
+  encoder.setRepeat(0);   // 0 for repeat, -1 for no-repeat
+  encoder.setDelay(100);  // frame delay in ms
+  encoder.setQuality(10); // image quality. 10 is default.
+
+  // use node-canvas
+  const canvas = document.createElement('canvas');
+  canvas.height = 320;
+  canvas.width = 240;
+  const ctx = canvas.getContext('2d');
+
+  for (var i=0; i<images.length; i++) {
+    encoder.addFrame(images[i]);
+  }
+
+  encoder.finish();
+
+  encoder.download("download.gif");
 }
